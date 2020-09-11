f8ae143f655904f51431c8b97d9678220ad92577
Paul Ccari <paulclindo@gmail.com>
2020-09-11 12:14:50 -0500

    fixing timings

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 export const ICONS = ["fish", "poop", "weather"];
-export const TICK_RATE = 1000;
-export const RAIN_CHANCE = 0.4;
+export const TICK_RATE = 2500;
+export const RAIN_CHANCE = 0.6;
 export const SCENES = ["day", "rain"];
-export const DAY_LENGTH = 60;
-export const NIGHT_LENGTH = 4;
+export const DAY_LENGTH = 30;
+export const NIGHT_LENGTH = 6;
 
 export const getNextHungerTime = (clock) =>
   Math.floor(Math.random() * 3) + 5 + clock; // 5,6,7 + clock
